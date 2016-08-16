ba1998f5f00529d63904f9d28b92fea2bbb3e1d6
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-15 22:17:40 -0600

    Add ideas to table via ajax

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+  $.ajax({
+      url: '/ideas',
+      method: "GET",
+      dataType: "JSON"
+  }).done(function(allIdeas){
+    $.each(allIdeas.ideas, function(i, idea) {
+      $('#ideas-table').append('<tr>' +
+                        '<td>' + idea.title + '</td>' +
+                        '<td>' + idea.body + '</td>' +
+                        '<td>' + idea.quality + '</td>' +
+                        '</tr>');
+    });
+  });
 });
