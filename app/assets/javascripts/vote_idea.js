5562379486aec2c17c8662c5dda128f9edd3ab90
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-17 16:27:48 -0600

    Remove repetition in voting

--- app/assets/javascripts/vote_idea.js
+++ app/assets/javascripts/vote_idea.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
-  $("#ideas-container").on("click", ".upvote-idea", function(){
+  $("#ideas-container").on("click", ".vote-idea", function(){
     var $ideaDiv = $(this).parent();
     var ideaID = $ideaDiv.data("id");
     var ideaQuality = $ideaDiv.children(".idea-box-quality").data("value");
 
-    if (ideaQuality < (qualities.length - 1)) {
-      var newQuality = ideaQuality + 1;
-      updateQuality(ideaID, $ideaDiv, newQuality);
-    };
-  });
-
-  $("#ideas-container").on("click", ".downvote-idea", function(){
-    var $ideaDiv = $(this).parent();
-    var ideaID = $ideaDiv.data("id");
-    var ideaQuality = $ideaDiv.children(".idea-box-quality").data("value");
-
-    if (ideaQuality > 0) {
-      var newQuality = ideaQuality - 1;
-      updateQuality(ideaID, $ideaDiv, newQuality);
+    if ($(this).hasClass("up")) {
+      if (ideaQuality < (qualities.length - 1)) {
+        var newQuality = ideaQuality + 1;
+        updateQuality(ideaID, $ideaDiv, newQuality);
+      };
+    } else {
+      if (ideaQuality > 0) {
+        var newQuality = ideaQuality - 1;
+        updateQuality(ideaID, $ideaDiv, newQuality);
+      };
     };
   });
 });
