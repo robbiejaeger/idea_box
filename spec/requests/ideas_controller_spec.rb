2dfcde89295db7a1e5358022483d32f866f85d4a
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-16 22:01:30 -0600

    Add delete request test

--- spec/requests/ideas_controller_spec.rb
+++ spec/requests/ideas_controller_spec.rb
@@ -22,16 +22,28 @@ describe "Ideas API Controller" do
     idea1 = Idea.create(title: "Idea1", body: "Idea1 body.")
 
     post '/ideas', params: {idea: {title: "New Idea!", body: "New body!"}}
-    expect(response.status).to eq(204)
+    expect(response.status).to eq(200)
 
     get '/ideas'
     parsed_response = JSON.parse(response.body)
-    
+
     expect(parsed_response["ideas"].length).to eq(2)
     expect(parsed_response["ideas"][0]["title"]).to eq("Idea1")
     expect(parsed_response["ideas"][0]["body"]).to eq("Idea1 body.")
 
     expect(parsed_response["ideas"][1]["title"]).to eq("New Idea!")
     expect(parsed_response["ideas"][1]["body"]).to eq("New body!")
+  end
+
+  it "can delete an idea" do
+    idea = Idea.create(title: "Idea", body: "Idea body.")
+
+    delete "/ideas/#{idea.id}"
+    expect(response.status).to eq(204)
+
+    get '/ideas'
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response["ideas"].length).to eq(0)
   end
 end
