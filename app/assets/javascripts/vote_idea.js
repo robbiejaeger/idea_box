925a5c3a1222c24881e8f639385b8c165ad398ad
Robbie Jaeger <jaeger.rob@gmail.com>
2016-08-16 23:39:51 -0600

    User can downvote an idea

--- app/assets/javascripts/vote_idea.js
+++ app/assets/javascripts/vote_idea.js
@@ -4,8 +4,27 @@ $(document).ready(function() {
     var ideaID = $ideaDiv.data("id");
     var ideaQuality = $ideaDiv.children(".idea-box-quality").data("value");
 
-    if (ideaQuality < 3) {
+    if (ideaQuality < 2) {
       var newQuality = ideaQuality + 1;
+
+      $.ajax({
+        url: '/ideas/' + ideaID,
+        method: "PATCH",
+        dataType: "JSON",
+        data: {idea: {quality: newQuality}}
+      }).done(function(){
+        $ideaDiv.children(".idea-box-quality").html(qualities[newQuality]);
+      });
+    };
+  });
+
+  $("#ideas-container").on("click", ".downvote-idea", function(){
+    var $ideaDiv = $(this).parent();
+    var ideaID = $ideaDiv.data("id");
+    var ideaQuality = $ideaDiv.children(".idea-box-quality").data("value");
+
+    if (ideaQuality > 0) {
+      var newQuality = ideaQuality - 1;
 
       $.ajax({
         url: '/ideas/' + ideaID,
