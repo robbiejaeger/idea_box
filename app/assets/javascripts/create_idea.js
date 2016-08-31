878c0c78346b1ca7ad58550512ac33b287ac8f7a
Nick Pisciotta <npisciotta@gmail.com>
2016-08-31 11:22:24 -0600

    Clean up create idea function

--- app/assets/javascripts/create_idea.js
+++ app/assets/javascripts/create_idea.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $("#create-idea").on("click", function(){
-    var ideaTitle = $("#idea-title").val();
-    var ideaBody = $("#idea-body").val();
+    var $ideaTitle = $("#idea-title");
+    var title = $ideaTitle.val();
+    var $ideaBody = $("#idea-body");
+    var body = $ideaBody.val();
 
     $.ajax({
       url: '/ideas',
       method: "POST",
       dataType: "JSON",
-      data: {idea: {title: ideaTitle, body: ideaBody}}
+      data: {idea: {title: title, body: body}}
     }).done(function(idea){
       renderIdea(idea.id, idea.title, idea.body, idea.quality);
 
-      $("#idea-title").val("");
-      $("#idea-body").val("");
+      $ideaTitle.val("");
+      $ideaBody.val("");
     });
   });
 });
